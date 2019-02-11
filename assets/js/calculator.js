2768276bce2989264d0b43b30b2396631ac3e20c
NasipCOM <salmahunov@mail.ru>
2019-02-11 18:29:07 +0600

    Calculator functionality

--- assets/js/calculator.js
+++
@@ -1,9 +0,0 @@
-var weight,length,height,width,scopeWeight;
-
-
-weight = document.querySelector("#num-weight").textContent;
-length = document.querySelector("#num-length").textContent;
-height= document.querySelector("#num-height").textContent;
-width = document.querySelector("#num-width").textContent;
-
-scopeWeight = length * height * width / 5000;
